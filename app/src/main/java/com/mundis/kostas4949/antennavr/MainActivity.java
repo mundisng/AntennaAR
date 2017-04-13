a0bafc1766455e8f06f8e18d7bf5333800309744
mundisng <mundisng@gmail.com>
2017-04-14 00:31:58 +0300

    settings toolbar, not working properly after welcome screen

--- app/src/main/java/com/mundis/kostas4949/antennavr/MainActivity.java
+++ app/src/main/java/com/mundis/kostas4949/antennavr/MainActivity.java
@@ -13,6 +13,9 @@ import android.location.LocationManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.SurfaceView;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
@@ -28,6 +31,7 @@ import java.util.TimerTask;
  */
 
 public class MainActivity extends AppCompatActivity{
+    private static Toolbar myToolbar;
     double x,y,z;
     int gps_data=0;
     boolean gps_enabled = false;
@@ -45,6 +49,8 @@ public class MainActivity extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ar);
+        myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
         sensorManager = (SensorManager) this.getSystemService(SENSOR_SERVICE);
         cameraContainerLayout = (FrameLayout) findViewById(R.id.camera_container_layout);
         surfaceView = (SurfaceView) findViewById(R.id.surface_view);
@@ -77,6 +83,31 @@ public class MainActivity extends AppCompatActivity{
 
 
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.my_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                // User chose the "Settings" item, show the app settings UI...
+                Toast.makeText(MainActivity.this, "Settings Pressed MAIN", Toast.LENGTH_SHORT).show();
+                return true;
+
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
+    }
+
     @Override
     public void onResume() {
         super.onResume();
--- app/src/main/java/com/mundis/kostas4949/antennavr/WelcomeActivity.java
+++ app/src/main/java/com/mundis/kostas4949/antennavr/WelcomeActivity.java
@@ -4,13 +4,20 @@ import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class WelcomeActivity extends AppCompatActivity {
-    private static int TIME_OUT = 4000;
+    private static int TIME_OUT = 14000;
+    private static Toolbar myToolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+        myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
@@ -19,5 +26,29 @@ public class WelcomeActivity extends AppCompatActivity {
                 finish();
             }
         }, TIME_OUT);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.my_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                // User chose the "Settings" item, show the app settings UI...
+                Toast.makeText(WelcomeActivity.this, "Settings Pressed WELCOME", Toast.LENGTH_SHORT).show();
+                return true;
+
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
     }
 }
