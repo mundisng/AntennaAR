9d9743b54c6c399ad59109fc92082ff1497721d9
mundisng <mundisng@gmail.com>
2017-04-17 22:08:06 +0300

    toolbar in main

--- app/src/main/java/com/mundis/kostas4949/antennavr/MainActivity.java
+++ app/src/main/java/com/mundis/kostas4949/antennavr/MainActivity.java
@@ -108,7 +108,9 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
+
         getMenuInflater().inflate(R.menu.my_menu, menu);
+        System.out.println("WAJDHKJASHNFJASFJKASLFKHALSJKF");
         return true;
     }
 
