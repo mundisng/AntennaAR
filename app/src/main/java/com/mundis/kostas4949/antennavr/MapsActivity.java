9cd2c74b0ea7495bef3e650a36b46cfaec8fc54e
mundisng <mundisng@gmail.com>
2017-04-25 01:28:10 +0300

    rotation not working?

--- app/src/main/java/com/mundis/kostas4949/antennavr/MapsActivity.java
+++ app/src/main/java/com/mundis/kostas4949/antennavr/MapsActivity.java
@@ -1,9 +1,15 @@
 package com.mundis.kostas4949.antennavr;
 
 import android.content.Context;
+import android.hardware.GeomagneticField;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.opengl.Matrix;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.widget.Toast;
@@ -12,15 +18,29 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, SensorEventListener {
 
     private GoogleMap mMap;
     boolean gps_enabled = false;
     LocationManager locationManager;
     double x,y,z;
+
+    private SensorManager mSensorManager;
+    private Sensor mCompass,mCompass1,mCompass2;
+    boolean rotation_compatibility=false;
+    private boolean ble;
+    private final float[] mAccelerometerReading = new float[3];
+    private final float[] mMagnetometerReading = new float[3];
+    private final float[] mRotationMatrix = new float[16];
+    private final float[] rotationMatrixFromVector = new float[16];
+    private float[] rotation_matrix;
+    private GeomagneticField field;
+    private float mDeclination;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,6 +57,19 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         } catch (SecurityException e) {
             Toast.makeText(getApplicationContext(),"Can't get gps location(security exception). Check your settings!",Toast.LENGTH_SHORT).show();
         }
+        mSensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
+
+        mCompass=mSensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR);
+        if (mCompass==null || mCompass.getMinDelay()==0){
+            System.out.println("Going into compatibility mode");
+            rotation_compatibility=true;
+            mCompass1=mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+            mCompass2=mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+        }
+        else {
+            System.out.println("Min delay:"+mCompass.getMinDelay());
+        }
+
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
@@ -60,20 +93,107 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         LatLng myloc = new LatLng(x, y);
         mMap.addMarker(new MarkerOptions().position(myloc).title("You are here!"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(myloc));
+        //updateCameraBearing(mMap,rotation_matrix);
     }
     ////////////////////////////////////////////////////////////
 
+    private void updateCameraBearing(GoogleMap googleMap, float bearing) {
+        if ( googleMap == null) return;
+        CameraPosition camPos = CameraPosition
+                .builder(
+                        googleMap.getCameraPosition() // current Camera
+                )
+                .bearing(bearing)
+                .build();
+        googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(camPos));
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (!rotation_compatibility) {
+            ble = mSensorManager.registerListener(this, mCompass, SensorManager.SENSOR_DELAY_FASTEST);
+
+            if (ble == true) {
+                System.out.println("Why is is true?");
+            } else {
+                System.out.println("ha ha,false!");
+            }
+        }
+        else {
+            ble=mSensorManager.registerListener(this, mCompass1,
+                    SensorManager.SENSOR_DELAY_FASTEST);
+            if (ble==true){
+                System.out.println("Why is is true?1");
+            }
+            else {
+                System.out.println("ha ha,false!1");
+            }
+            ble=mSensorManager.registerListener(this, mCompass2,
+                    SensorManager.SENSOR_DELAY_FASTEST);
+            if (ble==true){
+                System.out.println("Why is is true?2");
+            }
+            else {
+                System.out.println("ha ha,false!2");
+            }
+
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mSensorManager.unregisterListener(this);
+    }
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+    }
+
+    public void onSensorChanged(SensorEvent sEvent) {
+        float[] orientation = new float[3];
+        if (rotation_compatibility) {
+            if (sEvent.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+                System.arraycopy(sEvent.values, 0, mAccelerometerReading,
+                        0, mAccelerometerReading.length);
+            } else if (sEvent.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD) {
+                System.arraycopy(sEvent.values, 0, mMagnetometerReading,
+                        0, mMagnetometerReading.length);
+            }
+            if (mSensorManager.getRotationMatrix(mRotationMatrix, null,
+                    mAccelerometerReading, mMagnetometerReading)) {
+                SensorManager.getOrientation(mRotationMatrix, orientation);
+                float bearing = (float)Math.toDegrees(orientation[0]) + mDeclination;
+                updateCameraBearing(mMap,bearing);
+                rotation_matrix=mRotationMatrix;
+            }
+        }
+        else {
+            if (sEvent.sensor.getType() == Sensor.TYPE_ROTATION_VECTOR) {
+                SensorManager.getRotationMatrixFromVector(rotationMatrixFromVector, sEvent.values); //Get rotation of cell phone
+                SensorManager.getOrientation(rotationMatrixFromVector, orientation);
+                float bearing = (float)Math.toDegrees(orientation[0]) + mDeclination;
+                updateCameraBearing(mMap,bearing);
+                rotation_matrix=rotationMatrixFromVector;
+            }
+        }
+    }
 
     LocationListener locationListenerGps = new LocationListener() {
         public void onLocationChanged(Location location) {
             System.out.println("Calculating gps position...");
             if (location != null) {
-                x = location.getLatitude();
-                y = location.getLongitude();
-                z = location.getAltitude();
-                //gps_data=1;
-                Toast.makeText(getApplicationContext(),"location (gps) : " + x + " " + y + "and altitude: " + z,Toast.LENGTH_SHORT).show();
-                System.out.println("(GPS)x is: " + x + "y is: " + y);
+                field = new GeomagneticField(
+                        (float)location.getLatitude(),
+                        (float)location.getLongitude(),
+                        (float)location.getAltitude(),
+                        System.currentTimeMillis()
+                );
+                x=location.getLatitude();
+                y=location.getLongitude();
+                // getDeclination returns degrees
+                mDeclination = field.getDeclination();
+                //Toast.makeText(getApplicationContext(),"location (gps) : " + x + " " + y + "and altitude: " + z,Toast.LENGTH_SHORT).show();
+                //System.out.println("(GPS)x is: " + x + "y is: " + y);
 
             } else {
                 //gps_data=0;
